fb83a4ac006df76715cade59bfa5bd9040c3df76
Garrett Schmitt <81787496+schmitt530@users.noreply.github.com>
2022-03-11 16:37:27 -0500

    fixed issue with scroll effects not showing on IE

--- assets/js/add-active-on-scroll.js
+++ assets/js/add-active-on-scroll.js
@@ -3,10 +3,10 @@
 
  var activationsCompleted = 0;
 
- var toActivateOnScroll = Array.from(document.querySelectorAll('.activateOnScroll'));
+ var toActivateOnScroll = document.querySelectorAll('.activateOnScroll');
 
      for (var i = 0; i < toActivateOnScroll.length; i++) {
-         if (toActivateOnScroll[i].getBoundingClientRect().top <= (window.innerHeight - (window.innerHeight * .2))  && toActivateOnScroll[i].className.includes('active') == false) {
+         if (toActivateOnScroll[i].getBoundingClientRect().top <= (window.innerHeight - (window.innerHeight * .2))  && toActivateOnScroll[i].className.indexOf('active') == -1) {
            var toActivateOnScrollClasses = toActivateOnScroll[i].className;
            toActivateOnScroll[i].className = toActivateOnScrollClasses + " active";
            activationsCompleted++;
@@ -16,7 +16,7 @@
  function activateOnScroll() {
 
      for (var i = 0; i < toActivateOnScroll.length; i++) {
-         if (toActivateOnScroll[i].getBoundingClientRect().top <= (window.innerHeight - (window.innerHeight * .2)) && toActivateOnScroll[i].className.includes('active') == false) {
+         if (toActivateOnScroll[i].getBoundingClientRect().top <= (window.innerHeight - (window.innerHeight * .2)) && toActivateOnScroll[i].className.indexOf('active') == -1) {
            var toActivateOnScrollClasses = toActivateOnScroll[i].className;
            toActivateOnScroll[i].className = toActivateOnScrollClasses + " active";
            activationsCompleted++;
