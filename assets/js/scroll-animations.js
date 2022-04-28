cc2bb50761f02a58201aff579be26b42c66503da
Travis Brace <TravisBrace@gmail.com>
2022-04-28 14:22:03 -0400

    Update scroll-animations.js

--- assets/js/scroll-animations.js
+++ assets/js/scroll-animations.js
@@ -2,11 +2,11 @@
 
  var parallaxBackground = Array.from(document.querySelectorAll('.parallax-background'));
 
- var slideContainer = document.querySelector('.highlight .content');
+ // var slideContainer = document.querySelector('.highlight .content');
 
  //RUN ONCE TO SET INITAL PARALLAX SETTINGS
 
-document.querySelector('.highlight .parallax-background-child').setAttribute('style', 'background-position: 50% ' + (((slideContainer.getBoundingClientRect().top * 100) / window.innerHeight) / 2) + '%;');
+// document.querySelector('.highlight .parallax-background-child').setAttribute('style', 'background-position: 50% ' + (((slideContainer.getBoundingClientRect().top * 100) / window.innerHeight) / 2) + '%;');
 
  for (var i = 0; i < parallaxBackground.length; i++) {
 	parallaxBackground[i].setAttribute('style', 'background-position: 50% ' + (((parallaxBackground[i].getBoundingClientRect().top * 100) / window.innerHeight) / 2) + '%;');
@@ -16,7 +16,7 @@ document.querySelector('.highlight .parallax-background-child').setAttribute('st
  //THIS IS THE FUNCTION FOR PARALLAX SCROLLING
 
  function parallax() {
-    document.querySelector('.highlight .parallax-background-child').setAttribute('style', 'background-position: 50% ' + (((slideContainer.getBoundingClientRect().top * 100) / window.innerHeight) / 2) + '%;');
+//    document.querySelector('.highlight .parallax-background-child').setAttribute('style', 'background-position: 50% ' + (((slideContainer.getBoundingClientRect().top * 100) / window.innerHeight) / 2) + '%;');
 	for (var i = 0; i < parallaxBackground.length; i++) {
 	parallaxBackground[i].setAttribute('style', 'background-position: 50% ' + (((parallaxBackground[i].getBoundingClientRect().top * 100) / window.innerHeight) / 2) + '%;');
  }
